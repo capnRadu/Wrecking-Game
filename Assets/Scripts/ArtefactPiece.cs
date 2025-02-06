ba7684d3fe5afac037fe9efcd70f1a60cc7e64fe
Radu <aliment4476@gmail.com>
2025-02-06 15:01:18 +0100

    Random spawnpoints

---
+++ Assets/Scripts/ArtefactPiece.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtefactPiece : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> spawnPoints = new List<GameObject>();
+
+    private void Start()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Count);
+
+        while (spawnPoints[randomIndex].transform.childCount != 0)
+        {
+            randomIndex = Random.Range(0, spawnPoints.Count);
+        }
+
+        transform.position = spawnPoints[randomIndex].transform.position;
+        transform.SetParent(spawnPoints[randomIndex].transform);
+        spawnPoints.RemoveAt(randomIndex);
+    }
+}
